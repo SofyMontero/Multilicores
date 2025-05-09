82ab71eb351fff2d8879d73c46fc6d8ff7625db1
jarmandoR <jose523a@gmail.com>
2025-05-08 21:18:50 -0500

    wip: Se agrega imagenes a las categorias

--- views/categorias.php
+++ views/categorias.php
@@ -2,15 +2,44 @@
 
 <div class="container mt-5">
     <h2 class="text-center mb-4"><i class="fas fa-tags"></i> Selecciona una Categoría</h2>
-    <div class="row text-center">
-        <div class="col-md-4 mb-3">
-            <a href="catalogo.php?categoria=ron" class="btn btn-dark btn-lg w-100">Rones</a>
+    <div class="row">
+        <!-- RON -->
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm">
+                <a href="catalogo.php?categoria=ron">
+                    <img src="https://cdn.pixabay.com/photo/2020/03/20/15/50/rum-4951796_960_720.jpg" class="card-img-top" alt="Rones">
+                </a>
+                <div class="card-body text-center">
+                    <h5 class="card-title">Rones</h5>
+                    <a href="catalogo.php?categoria=ron" class="btn btn-dark w-100">Ver productos</a>
+                </div>
+            </div>
         </div>
-        <div class="col-md-4 mb-3">
-            <a href="catalogo.php?categoria=cerveza" class="btn btn-warning btn-lg w-100">Cervezas</a>
+
+        <!-- CERVEZA -->
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm">
+                <a href="catalogo.php?categoria=cerveza">
+                    <img src="https://cdn.pixabay.com/photo/2017/05/07/08/56/beer-2293436_960_720.jpg" class="card-img-top" alt="Cervezas">
+                </a>
+                <div class="card-body text-center">
+                    <h5 class="card-title">Cervezas</h5>
+                    <a href="catalogo.php?categoria=cerveza" class="btn btn-warning w-100">Ver productos</a>
+                </div>
+            </div>
         </div>
-        <div class="col-md-4 mb-3">
-            <a href="catalogo.php?categoria=aguardiente" class="btn btn-success btn-lg w-100">Aguardientes</a>
+
+        <!-- AGUARDIENTE -->
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm">
+                <a href="catalogo.php?categoria=aguardiente">
+                    <img src="https://cdn.pixabay.com/photo/2022/03/15/16/55/liquor-7070651_960_720.jpg" class="card-img-top" alt="Aguardientes">
+                </a>
+                <div class="card-body text-center">
+                    <h5 class="card-title">Aguardientes</h5>
+                    <a href="catalogo.php?categoria=aguardiente" class="btn btn-success w-100">Ver productos</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
