c6d9d7ca12b83284dc82014add39afaca8aa59b9
jarmandoR <jose523a@gmail.com>
2025-05-23 01:29:20 -0500

    wip: productoController

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -1,6 +1,10 @@
 <?php
-require_once "../vendor/autoload.php";      // PhpSpreadsheet
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once "../models/ProductoModel.php";      // Modelo
+require_once "../vendor/autoload.php";      // PhpSpreadsheet
+
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
