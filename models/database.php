bed7814f024654092f816c287773e4efc4a18e85
Sofia Montero <smontero@innapsis.com.co>
2025-05-31 00:36:20 -0500

    fix: cambio coneccion base de datos

--- models/database.php
+++ models/database.php
@@ -1,15 +1,15 @@
 <?php
 class Database {
-    // private $host = "localhost";
-    // private $db_name = "u633742531_Multilicores";
-    // private $username = "u633742531_Multilicores25";
-    // private $password = "Multilicores2025";
-    // private $charset = "utf8mb4";
     private $host = "localhost";
-    private $db_name = "multilicores";
-    private $username = "root";
-    private $password = "";
+    private $db_name = "u633742531_Multilicores";
+    private $username = "u633742531_Multilicores25";
+    private $password = "Multilicores2025";
     private $charset = "utf8mb4";
+    // private $host = "localhost";
+    // private $db_name = "multilicores";
+    // private $username = "root";
+    // private $password = "";
+    // private $charset = "utf8mb4";
 
     public function connect() {
         try {
