b4ec21b4f859b45d29888edb4c3c2fb3933f7be2
jarmandoR <jose523a@gmail.com>
2025-06-05 00:16:37 -0500

    wip: Enviar whatsapp de confirmacion

--- views/solicitudes.php
+++ views/solicitudes.php
@@ -161,9 +161,10 @@ $pedidos_rechazados = $solicitudModel->obtenerPedidosRechazados();
                                             </td>
                                             <td class="text-center">
                                                 <button class="btn btn-success btn-sm aceptar-pedido" data-pedido="<?php echo $pedido['id_pedido']; ?>">
-                                                    <input type="hidden" id="numCliente<?php echo $pedido['id_pedido']; ?>" data-numcliente="<?php echo$pedido['ped_numCliente']; ?>" />
                                                     <i class="fas fa-check"></i> Aceptar
                                                 </button>
+                                                <input type="hidden" id="numCliente<?php echo $pedido['id_pedido']; ?>" data-numcliente="<?php echo$pedido['ped_numCliente']; ?>" />
+
                                                 <button class="btn btn-danger btn-sm rechazar-pedido" data-pedido="<?php echo $pedido['id_pedido']; ?>">
                                                     <i class="fas fa-times"></i> Rechazar
                                                 </button>
