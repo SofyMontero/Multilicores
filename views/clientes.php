d20aec9c6a8708fbdbb02181a1651ea747165c61
Sofia Montero <smontero@innapsis.com.co>
2025-06-12 05:21:20 -0500

    fix: Ocultar errores PHP en la vista de clientes

--- views/clientes.php
+++ views/clientes.php
@@ -2,7 +2,7 @@
 // Mostrar errores en entorno de desarrollo (comentarlos en producción)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(0);
 
 include_once "header.php";
 require_once "../models/database.php";
