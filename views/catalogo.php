60655b20a08344080ab8f375a90d943bea30498f
jarmandoR <jose523a@gmail.com>
2025-07-01 13:09:42 -0500

    wip: se agrega icono pequeño de pestaña en catalogo

--- views/catalogo.php
+++ views/catalogo.php
@@ -23,7 +23,7 @@ $productos = $producto->obtenerProductos($categoria, $busqueda, $limit, $offset)
 
 <!DOCTYPE html>
 <html lang="es">
-
+<link rel="icon" type="image/png" href="../assets/img/logoM.png">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
