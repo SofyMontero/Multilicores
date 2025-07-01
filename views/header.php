a7c498ab50599db10d699f11975eba21354b787f
jarmandoR <jose523a@gmail.com>
2025-07-01 13:07:47 -0500

    wip: Se agrega icono pequeño de pestaña

--- views/header.php
+++ views/header.php
@@ -9,6 +9,7 @@ date_default_timezone_set('America/Bogota');
 ?>
 <!DOCTYPE html>
 <html lang="es">
+<link rel="icon" type="image/png" href="../assets/img/logoM.png">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
