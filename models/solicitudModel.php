9c2394171ef20953177cd1b0f9c814ff95ff145c
Sofia Montero <smontero@innapsis.com.co>
2025-07-02 21:01:22 -0500

    fix: Solicitud model date range

--- models/solicitudModel.php
+++ models/solicitudModel.php
@@ -34,6 +34,7 @@ class solicitud
                  OR ped_estado = 1
              ) 
              AND ped_fecha >= CURDATE()
+             AND ped_fecha < CURDATE() + INTERVAL 1 DAY
              ORDER BY ped_fecha DESC
             ");
         $query->execute();
